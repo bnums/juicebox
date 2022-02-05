bbf0ec2dfebf01d1573ee1da8736793f42fab0f8
Bryan Num <bnum33@gmail.com>
2022-02-04 23:51:42 -0800

    wrapping up

--- db/index.js
+++ db/index.js
@@ -3,7 +3,10 @@ const { Client } = require('pg'); // imports the pg module
 
 require('dotenv').config();
 // supply the db name and location of the database
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 
 async function getAllUsers() {
