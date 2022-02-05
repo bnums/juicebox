3581b6637b2bddde1dc1974921030ea43c9fe64b
Bryan Num <bnum33@gmail.com>
2022-02-04 16:25:59 -0800

    added POST and PATCH routes for posts.js

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost, getPostById, updatePost } = require('../db');
+const { requireUser } = require('./utils');
+
+//Web token stuff
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { JWT_SECRET } = process.env;
+
 
 postsRouter.use((res, req, next) => {
   console.log("A request is being made to /posts")
@@ -12,6 +19,74 @@ postsRouter.get('/', async (req, res, next) => {
   res.send({
     "posts": [posts]
   });
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+
+    // this will create the post and the tags for us
+    const post = await createPost(postData);
+  
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+    if(!post) {
+      next()
+    } else {
+      res.send({post});
+    };
+   
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 
--- api/tags.js
+++ api/tags.js
@@ -14,5 +14,17 @@ tagsRouter.get('/', async (req, res, next) => {
   });
 });
 
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+
+  
+  try {
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+  }
+});
+
 
 module.exports = tagsRouter
